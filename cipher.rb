2d04ab058669873d1d590321fcbe2fb9bb20d4f7
Brendon <brendon.robey@gmail.com>
2014-01-20 18:00:22 +1100

    Clean up code

--- cipher.rb
+++ cipher.rb
@@ -1,6 +1,32 @@
-puts "Input string to be ciphered"
-string_array = gets.chomp.gsub!(/[^A-Za-z]+/,'').upcase!.scan(/.{1,5}/)
-string_array.map! do |string|
-string.ljust(5,'X')
+class Cipher 
+  def initialize(keystream)
+    @keystream = keystream
+    @char_hash = Hash[(65..90).map { |i| [i.char, i] }] 
+  end
+
+# convert input into 5 char words
+  def sanitize
+    puts "Input string to be ciphered"
+    string_array = gets.chomp.gsub!(/[^A-Za-z]+/,'').upcase!.scan(/.{1,5}/)
+  
+    string_array.map! do |string|
+      string.ljust(5,'X')
+    end
+ 
+    return string_array 
+  end
+
+  def process(s_arr)
+   keystream_num = convert_to_num(@keystream)
+   input_num_arr = convert_to_num(s_arr)
+   
+
+  end
+
+  def convert_to_num(s_arr)
+    num_array = s_arr.map { |c| (@char_hash[c]) }
+    num_array.map! { |i| i - 64 }
+  end
+
+
 end
-puts "The array is: " + string_array
